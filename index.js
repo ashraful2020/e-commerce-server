ef0a697ea430f899327ebe110ec6e56621a7af32
ashraful2020 <mr.ashraful2020@gmail.com>
2022-10-19 15:08:11 +0600

    upload multiple buffer images using sharp

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
 
- 
 // Apps middleware
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
-// use route and import all router from others file 
-app.use('/api/v1/product',require("./routes/product")); 
-app.use('/api/v1/user',require("./routes/user")); 
-app.use('/api/v1/flash-product',require("./routes/flash_product")); 
-app.use('/api/v1/latest-deal',require("./routes/latest_deal")); 
-app.use('/api/v1/category',require("./routes/category")); 
-app.use('/api/v1/popular-category',require("./routes/popular_category")); 
-app.use('/api/v1/search-by-category',require("./routes/search_by_category")); 
-// app.use('/api/v1/',require("")); 
-// app.use('/api/v1/',require("")); 
-
+// use route and import all router from others file
+app.use("/api/v1/product", require("./routes/product"));
+app.use("/api/v1/user", require("./routes/user"));
+app.use("/api/v1/flash-product", require("./routes/flash_product"));
+app.use("/api/v1/latest-deal", require("./routes/latest_deal"));
+app.use("/api/v1/category", require("./routes/category"));
+app.use("/api/v1/popular-category", require("./routes/popular_category"));
+app.use("/api/v1/search-by-category", require("./routes/search_by_category"));
+// app.use('/api/v1/',require(""));
+// app.use('/api/v1/',require(""));
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.wijwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -34,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     client.connect(() => console.log("Database connected"));
-    app.get("/api/v1", async (req, res) => {
+    app.get("/", async (req, res) => {
       res.send(
         `<h1 style='color:#99bbf2;font-size:70px; margin-top:20%; text-align:center'> Amar store server side</h1>`
       );
@@ -57,9 +55,13 @@ module.exports.client = {
   productCollection: client.db("amarstore").collection("product"),
   categoryCollection: client.db("amarstore").collection("category"),
   flashProductCollection: client.db("amarstore").collection("flashProduct"),
-  popularCategoryCollection: client.db("amarstore").collection("popularCategory"),
+  popularCategoryCollection: client
+    .db("amarstore")
+    .collection("popularCategory"),
   latestDealCollection: client.db("amarstore").collection("latestDeal"),
-  searchByCategoryCollection: client.db("amarstore").collection("searchByCategory"),
+  searchByCategoryCollection: client
+    .db("amarstore")
+    .collection("searchByCategory"),
   // productCollection: client.db("amarstore").collection("product"),
   // productCollection: client.db("amarstore").collection("product"),
 };
--- routes/product.js
+++ routes/product.js
@@ -2,14 +2,50 @@ const express = require("express");
 const { ObjectId } = require("mongodb");
 const router = express.Router();
 const root_route = require("../index");
-router.get('/', async (req, res) => {
-    const cursor = await root_route.client.productCollection.find({}).toArray();
-    res.send(cursor)
+const sharp = require("sharp");
+
+router.get("/", async (req, res) => {
+  const cursor = await root_route.client.productCollection.find({}).toArray();
+  res.send(cursor);
 });
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const cursor = await root_route?.client?.productCollection?.findOne({ _id:ObjectId(id) });
-     return res.send(cursor)
+router.get("/show-product", async (req, res) => {
+  const cursor = await root_route.client.productCollection
+    .find({})
+    .project({ _id: 1, name: 1, img: 1, price: 1,stock:1 })
+    .toArray();
+  res.send(cursor);
+});
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const cursor = await root_route?.client?.productCollection?.findOne({
+    _id: ObjectId(id),
+  });
+  return res.send(cursor);
 });
 
-module.exports = router;    
+router.post("/", async (req, res) => {
+  const productInfo = req.body;
+  const images = req?.files?.images;
+  const allImage = [];
+  // Upload Images and reduce file size using sharp
+  for (const img of images) {
+    const resizeImages = await sharp(img.data)
+      .resize({ height: 320, width: 540, fit: "fill" })
+      .jpeg({
+        quality: 100,
+      })
+      .toBuffer()
+      .then((data) => {
+        return data.toString("base64");
+      });
+    allImage.push(resizeImages);
+  }
+  const result = {
+    ...productInfo,
+    images: allImage,
+    createdAt: new Date().toDateString(),
+  };
+
+  res.send(result);
+});
+module.exports = router;

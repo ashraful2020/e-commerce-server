fdab3a5b992ef4004567be0464d2e96e42587127
ashraful2020 <mr.ashraful2020@gmail.com>
2022-11-08 22:44:23 +0600

    updated to heroku for searching

--- routes/search_by_category.js
+++ routes/search_by_category.js
@@ -4,9 +4,8 @@ const root_route = require("../index");
  
 router.get("/", async (req, res) => {  
     const name = req.query.name;
-    console.log(name)
-    const result = await root_route.client.productCollection.find({category:"Earphones"}).toArray()
-     return res.send(result);
+    const result = await root_route.client.productCollection.find({category:name}).toArray() 
+    return res.send(result);
 });
 
 module.exports = router;
